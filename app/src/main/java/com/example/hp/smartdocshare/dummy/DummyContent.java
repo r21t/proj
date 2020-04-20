c8078a7cf333a1a2525fbef6cd33769f18e68ccc
Venkata Harish Kumar Mamidi <harish.mamidi.kumar@gmail.com>
2020-04-20 13:33:51 +0530

    Dummy content updated

--- app/src/main/java/com/example/hp/smartdocshare/dummy/DummyContent.java
+++ app/src/main/java/com/example/hp/smartdocshare/dummy/DummyContent.java
@@ -1,11 +1,13 @@
 package com.example.hp.smartdocshare.dummy;
 
+
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
+/**b
  * Helper class for providing sample content for user interfaces created by
  * Android template wizards.
  * <p>
@@ -16,12 +18,12 @@ public class DummyContent {
     /**
      * An array of sample (dummy) items.
      */
-    public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+    private static final List<DummyItem> ITEMS = new ArrayList<>();
 
     /**
      * A map of sample (dummy) items, by ID.
      */
-    public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+    private static final Map<String, DummyItem> ITEM_MAP = new HashMap<>();
 
     private static final int COUNT = 25;
 
@@ -50,19 +52,17 @@ public class DummyContent {
         return builder.toString();
     }
 
-    /**
-     * A dummy item representing a piece of content.
-     */
     public static class DummyItem {
         public final String id;
         public final String content;
-        public final String details;
+        final String details;
 
-        public DummyItem(String id, String content, String details) {
+        DummyItem(String id, String content, String details) {
             this.id = id;
             this.content = content;
             this.details = details;
         }
+
 
         @Override
         public String toString() {
